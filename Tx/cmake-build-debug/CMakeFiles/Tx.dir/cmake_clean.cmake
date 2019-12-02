3761eb742cc47d94d684f9337b847314da5b335d
aerez <alonerez4@gmail.com>
2019-12-02 11:56:34 +0200

    test

--- Tx/cmake-build-debug/CMakeFiles/Tx.dir/cmake_clean.cmake
+++ Tx/cmake-build-debug/CMakeFiles/Tx.dir/cmake_clean.cmake
@@ -1,11 +1,11 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Tx.dir/main.cpp.o"
+  "CMakeFiles/Tx.dir/BasicFrame.cpp.o"
   "CMakeFiles/Tx.dir/CustomFrame.cpp.o"
   "CMakeFiles/Tx.dir/FileSender.cpp.o"
-  "CMakeFiles/Tx.dir/BasicFrame.cpp.o"
   "CMakeFiles/Tx.dir/KeepAliveFrame.cpp.o"
-  "Tx.pdb"
+  "CMakeFiles/Tx.dir/main.cpp.o"
   "Tx"
+  "Tx.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
